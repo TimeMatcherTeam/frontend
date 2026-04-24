a16df6a04b64279c12d47acdfe6ef2a7a8b76ba4
Andrew Goodman <goodsoul.andrew@mail.ru>
2026-04-24 17:18:02 +0500

    снва добавил прокси

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const path = require("path");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 const PORT = 3000;
+
+app.use(
+    "/api",
+    createProxyMiddleware({
+        target: "http://localhost:5000/api",
+        changeOrigin: true,
+    })
+);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
