93bcf0468999fd42aef56c8dd737c1f7939ffa83
Andrew Goodman <goodsoul.andrew@mail.ru>
2026-04-24 17:04:52 +0500

    перенаправление с /api на 5000/api, чтобы использовать единый порт

--- public/scripts/requests.js
+++ public/scripts/requests.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:5000/api";
+export const API_URL = "/api";
 
 export async function getAbilities() {
   const response = await fetch(`${API_URL}/abilities`)
--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = 3000;
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', createProxyMiddleware({
+    target: 'http://localhost:5000/api',
+    changeOrigin: true,
+}));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile("/public/pages/index.html", { root: __dirname });
-  res.status(200);
-})
+    res.sendFile("/public/pages/index.html", { root: __dirname });
+    res.status(200);
+});
 
 app.get("/group_meeting/:meetingId", (req, res) => {
-  res.sendFile("/public/pages/group_meeting.html", { root: __dirname });
-  res.status(200);
-})
+    res.sendFile("/public/pages/group_meeting.html", { root: __dirname });
+    res.status(200);
+});
 
 app.get("/profile", (req, res) => {
-  res.sendFile("/public/pages/profile.html", { root: __dirname });
-  res.status(200);
-})
-
+    res.sendFile("/public/pages/profile.html", { root: __dirname });
+    res.status(200);
+});
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
